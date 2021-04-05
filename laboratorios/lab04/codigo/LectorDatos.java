f2a35bc147e06fe42677bcac423c39a8894206dc
unknown <jprinconu@eafit.edu.co>
2021-04-04 21:54:41 -0500

    Codigo

---
+++ laboratorios/lab04/codigo/LectorDatos.java
@@ -0,0 +1,212 @@
+package Lab01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Esta clase se encarga de leer el archivo txt con las cordenadas y distancias
+ * @version 1
+ * @author Juan Pablo Rincon Usma | Julian Gómez Benitez
+ */
+public class LectorDatos
+{
+    DigraphAL Mapa; // Lugar donde se guardan las posiciones del mapa
+    LinkedList<Integer> nodos; // Aqui se almacenan las posiciones donde están los nodos.
+    /**
+     * Método constructor para poder representar el mapa.
+     */
+    public LectorDatos(){
+        nodos = leerNodos();
+        this.Mapa = new DigraphAL(nodos.size(), nodos);
+        leerArcos();
+    }
+    /**
+     * Método que se encarga de leer los lugares del archivo txt y devolverlos como una estructura de datos
+     * @return Devuelve una LinkedList de lugares, decidimos usar LinkedList porque para insertar elementos es O(1)
+     */
+    public LinkedList<Integer> leerNodos() {
+        try{
+            FileReader f  = new FileReader("src\\Lab01\\puentesColgantes.txt");
+            BufferedReader b = new BufferedReader(f);
+            String line;
+            LinkedList<Integer> nodos = new LinkedList<>();
+            while((line = b.readLine()) != null) {
+                String[] data = line.split(" ");
+                if(data[0].equals("Arcos.")){
+                    break;
+                }
+                if(data[0].matches("[0-9]+")){
+                    int id = Integer.parseInt(data[0]);
+                    nodos.add(id);
+                }
+            }
+            b.close();
+            return nodos;
+        }catch (IOException e) {
+            System.out.println("Asegurece de tener el documento ''puentesColgantes''");
+        }
+        return null;
+    }
+    /**
+     * Método que se encarga de leer las calles del archivo txt y devolverlos como una estructura de datos
+     * @return Devuelve una LinkedList de calles, decidimos usar LinkedList porque para insertar elementos es O(1)
+     */
+    public void leerArcos(){
+        try{
+            FileReader f  = new FileReader("src\\Lab01\\puentesColgantes.txt");
+            BufferedReader b = new BufferedReader(f);
+            String line;
+            LinkedList<Integer> distancias = new LinkedList<>();
+            boolean yaSePuedeAgregar = false;
+            while((line = b.readLine()) != null) {
+                String[] data = line.split(" ");
+                if(data[0].equals("Arcos.")){
+                    yaSePuedeAgregar = true;
+                }
+                if(data[0].matches("[0-9]+") && yaSePuedeAgregar) {
+                    int inicio = Integer.parseInt(data[0]);
+                    int llegada = Integer.parseInt(data[1]);
+                    int distancia = Integer.parseInt(data[2].replace(".0",""));
+                    Mapa.addArc(inicio,llegada,distancia);
+                }
+            }
+            b.close();
+        }catch (IOException e) {
+            System.out.println("Asegurece de tener el documento ''puentesColgantes''");
+        }
+        return;
+    }
+
+    /**
+     * Método encargado de devolver la posición en donde se encuentra un elemento en el grafo
+     * así podemos solucionar que los grafos no empiezan desde 0, se busca en que posición fue asignado.
+     * @param source es el nodo que se le busca en que posción esta asignado en el grafo.
+     * @return la posición donde se encuentra el nodo source en el grafo Mapa.
+     */
+    public int devolverPos(int source){
+        int pos = 0;
+        for (int i = 0; i < nodos.size(); i++) {
+            if(nodos.get(i) == source){
+                pos = i;
+                break;
+            }
+        }
+        return pos;
+    }
+
+    int costoMin = Integer.MAX_VALUE; // Costo minimo en las recursiones para poder evaluar los caminos
+    ArrayList<Integer> caminoGlobal; // Los nodos recorridos para poder llegar al de menor costo
+
+    /**
+     * Método encargado de encontrar el camino más corto entre 2 puntos de un grafo usando backtracking.
+     * @param grafo el grafo dado como listas de adyacencia.
+     * @param source el punto inicial a desde donde se empezará a recorrer.
+     * @param destination el punto final b donde se debe llegar.
+     * @param costo variable para poder ir contando el costo del camino actual.
+     * @param camino es el camino que se esta recorriendo actualmente.
+     */
+    public void caminoMasCorto(DigraphAL grafo,int source, int destination, int costo, ArrayList<Integer> camino){
+        if(costo > costoMin){ // Caso de backtracking.
+            return;
+        }
+        if(source == destination){ // Caso de parada de la recursión.
+            camino.add(destination);
+            caminoGlobal = camino;
+            costoMin = Math.min(costoMin, costo);
+            return;
+        }
+        int pos = devolverPos(source);
+        ArrayList<Integer> suce = grafo.getSuccessors(pos);
+        camino.add(source);
+        for (Integer a:suce){
+            int i=0;
+            if(a == 0){
+                if(camino.contains(10000)){
+                    continue;
+                }
+            }else{
+                if(!camino.contains(a)){
+                    int costoTemp = costo + grafo.grafo.get(pos).get(i).second;
+                    ArrayList<Integer> caminoTemp = new ArrayList<>(camino);
+                    caminoMasCorto(grafo,a,destination,costoTemp,caminoTemp);
+                }
+            }
+            i++;
+        }
+    }
+
+    /**
+     *
+     * @param tsp
+     */
+    public void findMinRoute(ArrayList<LinkedList<Pair<Integer,Integer>>> tsp)
+    {
+        int sum = 0;
+        int counter = 0;
+        int j = 0, i = 0;
+        int min = Integer.MAX_VALUE;
+        List<Integer> visitedRouteList
+                = new ArrayList<>();
+
+        // Starting from the 0th indexed
+        // city i.e., the first city
+        visitedRouteList.add(0);
+        int[] route = new int[tsp.size()];
+
+        // Traverse the adjacency
+        // matrix tsp[][]
+        while (i < tsp.size()
+                && j < tsp.get(i).size()) {
+
+            // Corner of the Matrix
+            if (counter >= tsp.get(i).size() - 1) {
+                break;
+            }
+
+            // If this path is unvisited then
+            // and if the cost is less then
+            // update the cost
+            if (j != i
+                    && !(visitedRouteList.contains(j))) {
+                if (tsp.get(i).get(j).second < min) {
+                    min = tsp.get(i).get(j).second;
+                    route[counter] = j + 1;
+                }
+            }
+            j++;
+
+            // Check all paths from the
+            // ith indexed city
+            if (j == tsp.get(i).size()) {
+                sum += min;
+                min = Integer.MAX_VALUE;
+                visitedRouteList.add(route[counter] - 1);
+                j = 0;
+                i = route[counter] - 1;
+                counter++;
+            }
+        }
+
+        // Update the ending city in array
+        // from city which was last visited
+        i = route[counter - 1] - 1;
+
+        for (j = 0; j < tsp.size(); j++) {
+
+            if ((i != j) && tsp.get(i).get(j).second < min) {
+                min = tsp.get(i).get(j).second;
+                route[counter] = j + 1;
+            }
+        }
+        sum += min;
+
+        // Started from the node where
+        // we finished as well.
+        System.out.print("Minimum Cost is : ");
+        System.out.println(sum);
+    }
+}
---
+++ laboratorios/lab04/codigo/Main.java
@@ -0,0 +1,26 @@
+package Lab01;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Esta clase se encarga de ejecutar el programa.
+ * @version 1
+ * @author Juan Pablo Rincon Usma | Julian Gómez Benitez
+ */
+public class Main {
+    /**
+     * Main desde donde se ejecuta los métodos para buscar el camino más corto entre 2 nodos.
+     * @param args
+     */
+    public static void main(String[] args) {
+        LectorDatos ld = new LectorDatos();
+        ArrayList<Integer> cam = new ArrayList<>();
+        ld.caminoMasCorto(ld.Mapa,10000,4,0,cam);
+        System.out.println(ld.caminoGlobal);
+        System.out.println(ld.costoMin);
+        ld.Mapa.grafo = ld.Mapa.invertirGrafo(ld.Mapa.grafo);
+        ld.findMinRoute(ld.Mapa.grafo);
+    }
+
+}
