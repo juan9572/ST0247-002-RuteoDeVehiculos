6775340fedacf7a8508e24b4436d3ce3f44551be
juan9572 <68928340+juan9572@users.noreply.github.com>
2021-04-19 12:38:12 -0500

    Sin el cero que no hace nada jiji

--- talleres/taller09/Taller9.java
+++ talleres/taller09/Taller9.java
@@ -36,7 +36,7 @@ public class Taller9 {
         for(int i = 1; i <= a.length(); i++){
             for(int j = 1; j <= b.length(); j++){
                 if(a.charAt(i-1) == b.charAt(j-1)){
-                    dp[i][j] = 0 + dp[i-1][j-1];
+                    dp[i][j] = dp[i-1][j-1];
                 }else{
                     dp[i][j] = 1 + Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
                 }
